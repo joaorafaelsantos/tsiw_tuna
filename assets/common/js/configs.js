a29bc0aa0d79022dd45cd40e2a5788e228eafe62
João Rafael Santos <joaorafael23@gmail.com>
2017-11-29 15:19:41 +0000

    Added partnerships, modal and fixed some bugs

--- assets/common/js/configs.js
+++ assets/common/js/configs.js
@@ -2,6 +2,7 @@
 
 let openToggle = false;
 let width = $(window).width();
+let closeMenu;
 
 $(function () {
     // Animate the toogle (open and close)
@@ -9,23 +10,26 @@ $(function () {
         if (!openToggle) {
             $(".gatunos-toggler .navbar-toggler-icon").css("background-image", "url('data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0xOSA2LjQxTDE3LjU5IDUgMTIgMTAuNTkgNi40MSA1IDUgNi40MSAxMC41OSAxMiA1IDE3LjU5IDYuNDEgMTkgMTIgMTMuNDEgMTcuNTkgMTkgMTkgMTcuNTkgMTMuNDEgMTJ6Ii8+CiAgICA8cGF0aCBkPSJNMCAwaDI0djI0SDB6IiBmaWxsPSJub25lIi8+Cjwvc3ZnPg==')");
             openToggle = true;
+            $("#content").css("opacity", "0.2");
         } else {
             $(".gatunos-toggler .navbar-toggler-icon").css("background-image", "url('data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz4KICAgIDxwYXRoIGQ9Ik0zIDE4aDE4di0ySDN2MnptMC01aDE4di0ySDN2MnptMC03djJoMThWNkgzeiIvPgo8L3N2Zz4=')");
             openToggle = false;
+            $("#content").css("opacity", "1");
         }
     });
 
     // Lock dropdown click on desktop to prevent bug while click and hover an element
-    if (width > 767) {
+    if (width > 1025) {
         $(".dropdown").click(function (e) {
             $('.dropdown-toggle').prop('disabled', true);
         });
     }
 
     // Close menu    
-    function closeMenu() {
+    closeMenu = function closeMenu() {
         $(".gatunos-toggler .navbar-toggler-icon").css("background-image", "url('data:image/svg+xml;base64,PHN2ZyBmaWxsPSIjRkZGRkZGIiBoZWlnaHQ9IjI0IiB2aWV3Qm94PSIwIDAgMjQgMjQiIHdpZHRoPSIyNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxwYXRoIGQ9Ik0wIDBoMjR2MjRIMHoiIGZpbGw9Im5vbmUiLz4KICAgIDxwYXRoIGQ9Ik0zIDE4aDE4di0ySDN2MnptMC01aDE4di0ySDN2MnptMC03djJoMThWNkgzeiIvPgo8L3N2Zz4=')");
         $("#navbarMenu").attr("class", "navbar-collapse collapse");
         openToggle = false;
+        $("#content").css("opacity", "1");
     }
 });
